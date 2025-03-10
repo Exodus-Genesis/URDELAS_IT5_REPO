4540bcf580ae373efe7d8d82d37d317880aa4c46
Exodus-Genesis <Urdelasedrico@gmail.com>
2025-03-10 15:40:20 +0800

    added changes to my game battleship_v1

--- Battleship/battleship_v1.js
+++ Battleship/battleship_v1.js
@@ -2,14 +2,11 @@ var location1 = 4;
 var location2 = 5;
 var location3 = 6;
 
-
-
 var guess;
 var hits = 0;
 var guesses = 0;
 
 var isSunk = false;
-
 
 while(isSunk == false){
     guess = prompt("Multiple Warships spotted prepare for battle! (enter number 0-6)");
@@ -20,14 +17,14 @@ while(isSunk == false){
         guesses = guesses + 1;
 
         if (guess == location1 || guess == location2 || guess == location3) {
-            hits = hits + 1
+            hits = hits + 1;
             
-            if (hits = 3) {
-                isSunk == true;
-                alert("not even abaddon would come back from that capital is ship destroyed!")
+            if (hits == 3) {
+                isSunk = true;
+                alert("not even abaddon would come back from that capital is ship destroyed!");
             }
         } else {
-            alert("You can do better than that!")
+            alert("You can do better than that!");
         }
     }
 
@@ -35,3 +32,5 @@ while(isSunk == false){
 }
 
 var stats = "Traitor warship has been reduced to ashes with " + guesses + "shells!!" + "Your total accuracy is" + (3/guesses)
+
+alert(stats)
