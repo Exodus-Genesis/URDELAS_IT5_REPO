e7fa92f484ac9b56ff59f245299bb78890231e2c
Exodus-Genesis <Urdelasedrico@gmail.com>
2025-03-24 15:18:07 +0800

    function Activity 1

---
+++ Activities/Function - activity/script.js
@@ -0,0 +1,14 @@
+function whatShallIwear(temp) {
+    if (temp < 60 ) {
+        console.log("wear a jacket");
+    }else if (temp < 70) {
+        console.log("wear a sweater");
+    }else {
+        console.log("wear a t-shirt");
+    }
+
+}
+
+whatShallIwear(80);
+whatShallIwear(60);
+whatShallIwear(50);
